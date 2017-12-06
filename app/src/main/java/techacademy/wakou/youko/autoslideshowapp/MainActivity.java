70a37696477d4485257a1e33145e83b8b7f69aac
youko-wakou <appu2pudding@gmail.com>
2017-12-06 18:54:45 +0900

    自動再生機能

--- app/src/main/java/techacademy/wakou/youko/autoslideshowapp/MainActivity.java
+++ app/src/main/java/techacademy/wakou/youko/autoslideshowapp/MainActivity.java
@@ -2,6 +2,7 @@ package techacademy.wakou.youko.autoslideshowapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewFlipper = (ViewFlipper) findViewById(R.id.flip);
+
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(2000);
     }
 }
